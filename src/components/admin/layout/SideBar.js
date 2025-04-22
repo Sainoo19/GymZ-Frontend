ea482c6089270e2a48b5678cee66160318beffc2
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2025-04-23 01:16:06 +0700

    Fix UI

--- src/components/admin/layout/SideBar.js
+++ src/components/admin/layout/SideBar.js
@@ -2,22 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as MyIcon } from "../../../assets/icons/main_icon.svg";
 import { ReactComponent as IconAllProduct } from "../../../assets/icons/Icon_TatCaSanPham.svg";
+import { ReactComponent as Dashboard_Icon } from "../../../assets/icons/Dashboard_Icon.svg";
+import { ReactComponent as OrderIcon } from "../../../assets/icons/Order_Icon.svg";
+import { ReactComponent as Arhive_Icon } from "../../../assets/icons/Arhive_Icon.svg";
+import { ReactComponent as Bag_Icon } from "../../../assets/icons/Bag_Icon.svg";
+import { ReactComponent as Materials_Icon } from "../../../assets/icons/Materials_Icon.svg";
+import { ReactComponent as User_Icon } from "../../../assets/icons/User_Icon.svg";
+import { ReactComponent as Customer_Icon } from "../../../assets/icons/Customer_Icon.svg";
+import { ReactComponent as Branch_Icon } from "../../../assets/icons/Branch_Icon.svg";
+import { ReactComponent as Discount_Icon } from "../../../assets/icons/Discount_Icon.svg";
+import { ReactComponent as MemberShip_Icon } from "../../../assets/icons/MemberShip_Icon.svg";
+import { ReactComponent as MemberShipInvoice_Icon } from "../../../assets/icons/MemberShipInvoice_Icon.svg";
+import { ReactComponent as Calendar_Icon } from "../../../assets/icons/Calendar_Icon.svg";
+import { ReactComponent as Key_Icon } from "../../../assets/icons/Key_Icon.svg";
 import { Menu, X } from "lucide-react";
 
 const navItems = [
-    { to: "/admin/", label: "TỔNG QUAN", icon: IconAllProduct },
+    { to: "/admin/", label: "TỔNG QUAN", icon: Dashboard_Icon },
     // { to: "/admin/inventory", label: "NHẬP KHO", icon: IconAllProduct },
-    { to: "/admin/orders", label: "ĐƠN HÀNG", icon: IconAllProduct },
-    { to: "/admin/payments", label: "HOÁ ĐƠN", icon: IconAllProduct },
-    { to: "/admin/productCategories", label: "DANH MỤC SẢN PHẨM", icon: IconAllProduct },
-    { to: "/admin/products", label: "SẢN PHẨM", icon: IconAllProduct },
-    { to: "/admin/employees", label: "NHÂN VIÊN", icon: IconAllProduct },
-    { to: "/admin/users", label: "KHÁCH HÀNG", icon: IconAllProduct },
-    { to: "/admin/branches", label: "CHI NHÁNH", icon: IconAllProduct },
-    { to: "/admin/discounts", label: "KHUYẾN MÃI", icon: IconAllProduct },
-    { to: "/admin/members", label: "HỘI VIÊN", icon: IconAllProduct },
-    { to: "/admin/member-bills", label: "HOÁ ĐƠN HỘI VIÊN", icon: IconAllProduct },
-    { to: "/admin/train-sessions", label: "LỊCH TẬP", icon: IconAllProduct }
+    { to: "/admin/orders", label: "ĐƠN HÀNG", icon: Arhive_Icon },
+    { to: "/admin/payments", label: "HOÁ ĐƠN", icon: OrderIcon },
+    { to: "/admin/productCategories", label: "DANH MỤC SẢN PHẨM", icon: Materials_Icon },
+    { to: "/admin/products", label: "SẢN PHẨM", icon: Bag_Icon },
+    { to: "/admin/employees", label: "NHÂN VIÊN", icon: User_Icon },
+    { to: "/admin/users", label: "KHÁCH HÀNG", icon: Customer_Icon },
+    { to: "/admin/branches", label: "CHI NHÁNH", icon: Branch_Icon },
+    { to: "/admin/discounts", label: "KHUYẾN MÃI", icon: Discount_Icon },
+    { to: "/admin/members", label: "HỘI VIÊN", icon: MemberShip_Icon },
+    { to: "/admin/member-bills", label: "HOÁ ĐƠN HỘI VIÊN", icon: MemberShipInvoice_Icon },
+    { to: "/admin/train-sessions", label: "LỊCH TẬP", icon: Calendar_Icon },
+    { to: "/admin/change-password-admin", label: "ĐỔI MẬT KHẨU", icon: Key_Icon }
 ];
 
 const SideBar = ({ isSidebarHidden, setIsSidebarHidden }) => {
@@ -44,7 +58,7 @@ const SideBar = ({ isSidebarHidden, setIsSidebarHidden }) => {
                                 onClick={() => setIsSidebarHidden(true)}
                                 className="text-text_color_secondary flex p-4 items-center group hover:bg-secondary hover:text-text_color_primary"
                             >
-                                <item.icon className="mr-2 stroke-current group-hover:stroke-secondary" />
+                                <item.icon className="mr-2 stroke-current " />
                                 {item.label}
                             </Link>
                         ))}
---
+++ src/pages/admin/privacy/ChangePasswordAdminPage.js
@@ -0,0 +1,12 @@
+import SidebarEmployeeProfile from '../../../components/admin/employee/SidebarEmployeeProfile';
+import PasswordChangeContentEmployee from '../../../components/admin/employee/PasswordChangeContentEmployees';
+
+function ChangePasswordPage() {
+    return (
+        <div className="flex min-h-screen bg-gray-100">
+            <PasswordChangeContentEmployee />
+        </div>
+    );
+}
+
+export default ChangePasswordPage;
